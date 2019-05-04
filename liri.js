5b28f8b43e32c4c1aa5d3cea3ed1e7fd2f99d30e
Steven Garza (eg22453) <stevenmacgarza@gmail.com>
2019-05-04 12:43:49 -0500

    finish the spotify app

--- liri.js
+++ liri.js
@@ -6,7 +6,6 @@ var fs = require("fs");
 
 //var spotify = new Spotify(keys.spotify);
 var spotify = new Spotify(keys.spotify);
-
 // Next, we store the text given to us from the command line.
 var input1 = process.argv[2];
 var input2 = process.argv[3];
@@ -22,127 +21,140 @@ for (var i = 4; i < process.argv.length; i++) {
 
 
 
-function userCommand(input1, input2) {
-    var input2 = input2.split(' ').join('+');
-    switch (input1) {
-        case "concert-this":
-            concertThis();
-            break;
-        case "spotify-this-song":
-            spotifyThisSong();
-            break;
-        case "movie-this":
-            movieThis();
-            break;
-        case "do-what-it-says":
-            dowhatitssays()
-            break;
-        default:
-            console.log("You have not entered a valid key")
-            break;
-    }
-}
-
-userCommand(input1, input2)
-
-function dowhatitssays() {
-    //Do the code for this method/command
-    fs.readFile("random.txt", "utf8", function (error, data) {
-        if (error) {
-            console.log(error)
-        }
-        var balanceArray = data.split(",");
-        input1 = balanceArray[0];
-        console.log(input1)
-        input2 = balanceArray[1]
-        console.log(input2)
-        userCommand(input1, input2)
-        // userchoice(input1,input2)
-    })
-}
-
-//movie-this <movie title> command, spaces need to have a + sign or %20
-function movieThis() {
-    //If no movie is input than Mr. Nobody will be the default movie
-    if (!input2) {
-        input2 = "Mr.+Nobody"
-    }
-    //console.log(input2)
-    axios.get("http://www.omdbapi.com/?t=" + input2 + "&y=&plot=short&apikey=trilogy").then(
-        function (response) {
-            console.log("The movie's Title is: " + response.data.Title);
-            console.log("The movie's premiere year is: " + response.data.Year);
-            console.log("The movie's IMBD rating is: " + response.data.Ratings[0].Value);
-            console.log("The movie's Rotten Tomatoes score is: " + response.data.Ratings[1].Value);
-            console.log("The movie's Country of origin is: " + response.data.Country);
-            console.log("The movie's inherent Language is: " + response.data.Language);
-            console.log("The movie's Plot: " + response.data.Plot);
-            console.log("The Actors in the Movie: " + response.data.Actors);
-        }
-
-    );
-
-}
-
-//function for finding concert information
-//called in switch statement if input 1 is appropriate
-function concertThis() {
-    axios.get("https://rest.bandsintown.com/artists/" + input2 + "/events?app_id=codingbootcamp").then(
-        function (response) {
-            //console.log();
-            //SPACES NEED TO BE ENCODED WITH A "%20" INSIDE OF THE URL
-            for (var i = 0; i < response.data.length; i++) {
-                console.log(response.data[i].venue.name)
-                console.log(response.data[i].venue.city)
-                console.log(response.data[i].datetime)
-                console.log("")
-            }
-            if (!input2) {
-                console.log("You need to type an artist to search for!")
-            }
-        }
-    );
-}
-
-
-
-//function to display spotifytracks
 function spotifyThisSong() {
-    spotify
-        .search({
+    spotify.search({
             type: 'track',
-            query: input2,
+            query: 'Rocket Man',
             limit: 1
-        })
-        .then(function (error, data) {
+        }, function (err, data) {
+            if (err) {
+                return console.log("Damage Critical: " + err);
+            }
+            //want to shorten this part up find a way to combine
+            console.log(data.tracks.items)
+            console.log("-------------------------------------\n" + "Song Name: " + data.tracks.items[0].name + "\n-------------------------------------\n" +
+                "The Song is by: " + data.tracks.items[0].artists[0].name +
+                "\n-------------------------------------\n" + "This Album of  the song is called: " +
+                data.tracks.items[0].album.name +
+                "\n-------------------------------------\n" + "Here's an external link to the song: " +
+                data.tracks.items[0].album.external_urls.spotify+
+                "\n-------------------------------------\n")
+            });
+    }
+
+    spotifyThisSong()
+
+    function userChoice(input1, input2) {
+        if (input2) {
+            var input2 = input2.split(' ').join('+');
+        }
+        switch (input1) {
+            case "concert-this":
+                concertThis();
+                break;
+            case "spotify-this-song":
+                spotifyThisSong();
+                break;
+            case "movie-this":
+                movieThis();
+                break;
+            case "do-what-it-says":
+                dowhatitssays()
+                break;
+            default:
+                console.log("You have not entered a valid key")
+                break;
+        }
+    }
+
+    userChoice(input1, input2)
+
+    function dowhatitssays() {
+        //Do the code for this method/command
+        fs.readFile("random.txt", "utf8", function (error, data) {
             if (error) {
                 console.log(error)
             }
-            console.log(data);
+            var balanceArray = data.split(",");
+            input1 = balanceArray[0];
+            console.log(input1)
+            input2 = balanceArray[1]
+            console.log(input2)
+            userChoice(input1, input2)
+            // userchoice(input1,input2)
         })
+    }
+
+    //movie-this <movie title> command, spaces need to have a + sign or %20
+    function movieThis() {
+        //If no movie is input than Mr. Nobody will be the default movie
+        if (!input2) {
+            input2 = "Mr.+Nobody"
+        }
+        //console.log(input2)
+        axios.get("http://www.omdbapi.com/?t=" + input2 + "&y=&plot=short&apikey=trilogy").then(
+            function (response) {
+                console.log("The movie's Title is: " + response.data.Title);
+                console.log("The movie's premiere year is: " + response.data.Year);
+                console.log("The movie's IMBD rating is: " + response.data.Ratings[0].Value);
+                console.log("The movie's Rotten Tomatoes score is: " + response.data.Ratings[1].Value);
+                console.log("The movie's Country of origin is: " + response.data.Country);
+                console.log("The movie's inherent Language is: " + response.data.Language);
+                console.log("The movie's Plot: " + response.data.Plot);
+                console.log("The Actors in the Movie: " + response.data.Actors);
+            }
+
+        );
+
+    }
+
+    //function for finding concert information
+    //called in switch statement if input 1 is appropriate
+    function concertThis() {
+        axios.get("https://rest.bandsintown.com/artists/" + input2 + "/events?app_id=codingbootcamp").then(
+            function (response) {
+                //console.log();
+                //SPACES NEED TO BE ENCODED WITH A "%20" INSIDE OF THE URL
+                for (var i = 0; i < response.data.length; i++) {
+                    console.log(response.data[i].venue.name)
+                    console.log(response.data[i].venue.city)
+                    console.log(response.data[i].datetime)
+                    console.log("")
+                }
+                if (!input2) {
+                    console.log("You need to type an artist to search for!")
+                }
+            }
+        );
+    }
 
 
-}
-// 2. `node liri.js spotify-this-song '<song name here>'`
 
-//    * This will show the following information about the song in your terminal/bash window
+    //function to display spotifytracks
+    // function spotifyThisSong() {
+    //     spotify
+    //         .search({
+    //             type: 'track',
+    //             query: input2,
+    //             limit: 1
+    //         })
+    //         .then(function (data) {
+    //             console.log(data);
+    //         })
+    //         .catch(function (err) {
+    //             console.log(err)
+    //         })
 
-//      * Artist(s)
 
-//      * The song's name
+    // }
+    // 2. `node liri.js spotify-this-song '<song name here>'`
 
-//      * A preview link of the song from Spotify
+    //    * This will show the following information about the song in your terminal/bash window
 
-//      * The album that the song is from
+    //      * Artist(s)
 
-//    * If no song is provided then your program will default to "The Sign" by Ace of Base.
-// spotify.search({
-//     type: 'track',
-//     query: 'All the Small Things'
-// }, function (err, data) {
-//     if (err) {
-//         return console.log('Error occurred: ' + err);
-//     }
+    //      * The song's name
 
-//     console.log(data);
-// });
+    //      * A preview link of the song from Spotify
+
+    //      * The album that the song is from
